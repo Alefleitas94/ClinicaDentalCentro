aff18f606d633494a0533cbba1d7c397dfab4254
Alejandro Fleitas <afleitasinfo@gmail.com>
2019-11-19 12:57:09 -0300

    agrego favicon a conocenos.html

---
+++ src/scripts/back_home.js
@@ -0,0 +1,14 @@
+
+  $("#back-servicio").on('click', function(event) {
+
+    location.href="index.html#servicio"
+  });
+  $("#back-tecnologia").on('click', function(event) {
+
+    location.href="index.html#tecnologia"
+  });
+
+  $("#back-contacto").on('click', function(event) {
+
+    location.href="index.html#contacto"
+  });
--- src/scripts/scrollspy.js
+++ src/scripts/scrollspy.js
@@ -22,19 +22,6 @@ $("#myNavbar a").on('click', function(event) {
     }  
   });
 
-  $("#back-servicio").on('click', function(event) {
-
-    location.href="index.html#servicio"
-  });
-  $("#back-tecnologia").on('click', function(event) {
-
-    location.href="index.html#tecnologia"
-  });
-
-  $("#back-contacto").on('click', function(event) {
-
-    location.href="index.html#contacto"
-  });
 
 
 
